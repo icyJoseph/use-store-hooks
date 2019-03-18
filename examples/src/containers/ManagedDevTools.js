ce5b710c35052b0b1c38b349d3c7380b7d5f20f3
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-03-18 07:43:55 +0100

    feat: add a React component which is connected to Redux dev tools

--- examples/src/containers/ManagedDevTools.js
+++ examples/src/containers/ManagedDevTools.js
@@ -1,27 +1,4 @@
-export const managedSrc = `import React, { Component } from "react";
-import Counter from "../components/Counter";
-import reducer, { INC, DEC } from "../ducks/counter";
-
-export class Managed extends Component {
-  state = {
-    count: 0
-  };
-
-  dispatch = action => reducer(this.state.count, action);
-
-  increase = () => this.setState({ count: this.dispatch({ type: INC }) });
-  decrease = () => this.setState({ count: this.dispatch({ type: DEC }) });
-
-  render() {
-    const { count } = this.state;
-    return <Counter count={count} inc={this.increase} dec={this.decrease} />;
-  }
-}
-
-export default Managed;
-`;
-
-export const managedDevToolsSrc = `import React, { Component } from "react";
+import React, { Component } from "react";
 import Counter from "../components/Counter";
 import reducer, { INC, DEC } from "../ducks/counter";
 
@@ -72,5 +49,3 @@ export class ManagedDevTools extends Component {
 }
 
 export default ManagedDevTools;
-
-`;
--- examples/src/index.js
+++ examples/src/index.js
@@ -10,8 +10,9 @@ import Main from "./containers/Main";
 import GlobalStore from "./containers/GlobalStore";
 import Enhancers from "./containers/Enhancers";
 import Managed from "./containers/Managed";
+import ManagedDevTools from "./containers/ManagedDevTools";
 
-import { managedSrc } from "./code";
+import { managedSrc, managedDevToolsSrc } from "./code";
 
 import "./index.css";
 import "./bootstrap.min.css";
@@ -43,6 +44,14 @@ function App() {
         >
           <Code code={managedSrc} />
           <Managed />
+        </Header>
+        <Header
+          id="managedDevTools"
+          title="Managed Dev Tools"
+          description="This counter uses a React Component, and controls its state using reducers. And it is connected to Redux Dev Tools."
+        >
+          <Code code={managedDevToolsSrc} />
+          <ManagedDevTools />
         </Header>
       </Container>
     </Fragment>
