c8bc407911f7f10df2c2627a4a39df70d6d00d61
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-03-28 11:31:45 +0100

    test: create a base test case for redux store usage

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,22 +1,81 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { compose } from "../src/compose";
-describe("Compose", () => {
-  const double = a => 2 * a;
-  it("composes", () => {
-    expect(
-      compose(
-        double,
-        double
-      )(2)
-    ).toEqual(8);
-  });
-});
+import React, { useContext } from "react";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
 
-const App = () => <div>Hi</div>;
+import { invokeStore, Provider, State } from "../src";
 
+// Prepare a test scenario
+const INC = "inc";
+const DEC = "dec";
+
+// define a common reducer
+function reducer(count = 0, { type, ...payload }) {
+  switch (type) {
+    case INC:
+      return count + 1;
+    case DEC:
+      return count - 1;
+    default:
+      return count;
+  }
+}
+
+// defined a counter component
+function Counter() {
+  const { state, dispatch } = useContext(State);
+  return (
+    <div>
+      <span>{state}</span>
+      <button onClick={() => dispatch({ type: INC })} />
+      <button onClick={() => dispatch({ type: DEC })} />
+    </div>
+  );
+}
+
+// invoke the store
+const store = invokeStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+
+// Run the tests!
 describe("React Comp", () => {
-  it("shallows", () => {
-    expect(shallow(<App />)).toBeDefined();
+  // mount the app
+  const app = mount(<App />);
+
+  // find the needed buttons
+  const incButton = app.find("button").first();
+  const decButton = app.find("button").last();
+
+  it("it is defined", () => {
+    expect(app).toBeDefined();
+  });
+
+  it("shows count initial state: 0", () => {
+    expect(app.find("span").text()).toEqual("0");
+  });
+
+  it("updates the counter: + 1", () => {
+    act(() => {
+      incButton.simulate("click");
+    });
+    expect(app.find("span").text()).toEqual("1");
+  });
+
+  it("updates the counter back to: 0", () => {
+    act(() => {
+      decButton.simulate("click");
+    });
+    expect(app.find("span").text()).toEqual("0");
+  });
+
+  it("updates the counter: -1", () => {
+    act(() => {
+      decButton.simulate("click");
+    });
+    expect(app.find("span").text()).toEqual("-1");
   });
 });
